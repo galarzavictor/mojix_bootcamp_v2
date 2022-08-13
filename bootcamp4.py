0830dcaefa89c3032f060a7bb7b730232dbd0a7a
galarzavictor <galarzavictor@gmail.com>
2022-08-13 10:59:33 -0400

    novena

--- bootcamp4.py
+++ bootcamp4.py
@@ -128,7 +128,30 @@ def run_fxn():
 st.write(run_fxn())	
 
 # Plot
-st.pyplot()
+with st.echo():
+	st.pyplot()
+	fig, ax = plt.subplots()
+	ax.scatter([1, 2, 3], [1, 2, 3])
+	   ... other plotting actions ...
+	st.pyplot(fig)
+
+import matplotlib.pyplot as plt
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+two_subplot_fig = plt.figure(figsize=(6,6))
+plt.subplot(211)
+plt.plot(t1, f(t1), color='tab:blue', marker=',')
+plt.plot(t2, f(t2), color='black', marker='.')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), color='tab:orange', linestyle='--', marker='.')
+
+st.pyplot(two_subplot_fig)
 
 # DataFrames
 st.dataframe(df)
